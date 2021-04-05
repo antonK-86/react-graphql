5fc1f05a6e985bcf4cf424e1416b9e2d28806dcc
antonK-86 <ank86@inbox.ru>
2021-04-05 11:43:28 +0300

    deploy app

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  uri: "http://salty-crag-83095.herokuapp.com/graphql",
+  uri: "https//salty-crag-83095.herokuapp.com/graphql",
 });
 
 function App() {
